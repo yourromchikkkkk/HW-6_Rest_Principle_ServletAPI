fa5fc4ad1193dec245b1efa1454ce4b946ce9ea4
unknown <romanmatsuiev@gmail.com>
2022-01-03 19:01:47 +0200

    commit

---
+++ src/main/java/com/cursor/rest/restapp/controller/UserController.java
@@ -0,0 +1,76 @@
+package com.cursor.rest.restapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.cursor.rest.restapp.model.User;
+
+import java.net.URI;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    List<User> users = new ArrayList<>();
+
+    public UserController() {
+        users.add(new User(1, "Roman", "Matsuiev", 19));
+        users.add(new User(2, "Maksim", "Hamulyak", 18));
+    }
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") int id) {
+        Optional<User> result = users.stream().filter(user -> user.getId() == id).findFirst();
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        Optional optionalUserName = users.stream().filter(temp -> Objects.equals(temp.getName(), user.getName())).findFirst();
+        Optional optionalUserId = users.stream().filter(temp -> temp.getId() == user.getId()).findFirst();
+        if (!optionalUserName.isPresent() && !optionalUserId.isPresent()) {
+            users.add(user);
+            return ResponseEntity.created(URI.create("/users")).build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("id") int id) {
+        Optional optionalUser = users.stream().filter(temp -> temp.getId() == id).findFirst();
+        if (optionalUser.isPresent()) {
+            for (User temp : users) {
+                if(temp.getId() == id) {
+                    temp.setAge(user.getAge());
+                    temp.setName(user.getName());
+                    temp.setSurname(user.getSurname());
+                }
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.accepted().build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") int id) {
+        Optional optionalUser = users.stream().filter(temp -> temp.getId() == id).findFirst();
+        if(optionalUser.isPresent()) {
+            users.remove(optionalUser.get());
+            return ResponseEntity.accepted().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/cursor/rest/restapp/model/User.java
@@ -0,0 +1,49 @@
+package com.cursor.rest.restapp.model;
+
+public class User {
+    int id;
+    String name;
+    String surname;
+    int age;
+
+    public User() {}
+
+    public User(int id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
